237f6cc26d489ee55abfefc5298d486926e2cf87
Roeland van Stee <roelandvanstee@gmail.com>
2022-04-03 17:58:10 +0200

    Test image

--- js/threejs.js
+++ js/threejs.js
@@ -1,7 +1,7 @@
 import * as THREE from "./threejs/three.module.js";
 import { OrbitControls } from "./threejs/OrbitControls.js";
 import { Interaction } from "./threejs/three.interaction.js";
-import earthImage from "../img/blue-world.png";
+// import earthImage from "../img/blue-world.png";
 
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
@@ -18,7 +18,7 @@ globe.rotateY(5.2);
 globe.rotateX(0.2);
 
 // Materials
-const texture = new THREE.TextureLoader().load(earthImage);
+const texture = new THREE.TextureLoader().load("../img/blue-world.png");
 const material = new THREE.MeshPhongMaterial({
     map: texture,
     transparent: true,
